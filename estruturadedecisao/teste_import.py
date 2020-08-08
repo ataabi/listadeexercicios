5cfb5f0778cfe4b8a21be83997db736cf26d2700
ataabi <jhony.prf@gmail.com>
2020-08-08 11:51:20 -0300

    Exercicios de python

--- estruturadedecisao/teste_import.py
+++ estruturadedecisao/teste_import.py
@@ -3,7 +3,7 @@ print('Digite 1 para Analize de Numeros')
 print('Digite 2 para Escolha de Genero')
 escolha = input(':? ')
 if escolha == 1:
-    from estrutura_de_decisao.decisao_1 import AnaNumerica
+    from estruturadedecisao.decisao_1 import AnaNumerica
 elif escolha == 2:
-    from estrutura_de_decisao.decisao_2 import escolhadegenero
+    from estruturadedecisao.decisao_2 import escolhadegenero
 
